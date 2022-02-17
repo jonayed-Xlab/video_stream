99bb5bf45fe42afac567e830156eeb7fb6b54999
jonayed153 <jonayed153@gmail.com>
2022-02-17 16:28:09 +0600

    stream app

---
+++ video-stream-platfrorm/src/test/java/io/stream/videostreamplatfrorm/VideoStreamPlatfrormApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stream.videostreamplatfrorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamPlatfrormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
